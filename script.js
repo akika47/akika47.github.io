93974b6fb6888df8ca60d30eb97edd7bd278fe73
akika47 <61090999+akika47@users.noreply.github.com>
2023-09-23 20:00:05 +0200

    kazanweblap

---
+++ script.js
@@ -0,0 +1,186 @@
+var language = {
+    en: {
+      dropdownHun: "Hungarian",
+      dropdownEng: "English",
+      navbarDropdown: "Language",
+      navbarSellers: "Resellers",
+      navbarContacts: "Contacts",
+      introHeader: "About us",
+      introduction: "Welcome to Uniholz ​​​​KFT. on your website! Uniholz ​​KFT. in the name of reliability and longevity, we offer a cheaper solution for heating the home. With our expertise and more than 25 years of experience in the field of mixed-fired boilers, we produce the highest and annual quality. We offer Delta mixed fuel boilers, with which we can solve heating problems with the smallest investment and the fastest.",
+      boilerHeader: "Combustion Boiler Family",
+      Boilers: "The advantages of Delta mixed-fuel boilers include large doors that facilitate handling. We offer an economical solution, as these boilers not only heat efficiently, but also utilize the ash that falls during heating. In addition, our products are also characterized by their aesthetic appearance, so our boilers are not only efficient, but also aesthetically pleasing in the home. Among the different types, you can choose the D-28, D-36 and D-48 models, all of which are perfectly adapted to different heating needs. You can use our mixed fuel boilers for both pump and gravity heating, so you can be sure that you will find the most suitable solution for you.",
+      sellersHeader: "Official resellers",
+
+      Központitelep: "Uniholz Kft. - Central site",
+      Miskolcizelep: "Uniholz Kft. - Miskolci site",
+      Debrecenitelep: "Keletker Vaskereskedés",
+      Nyíregyházitelep: "Nyír-Acél Kft.",
+      Békéscsabaitelep: "Delta-Metál Kft.",
+      Egritelep: "Uniholz Kft. - Egri site",
+      Szegeditelep: "Hungaroferro Vaskereskedés",
+      locationHeader: "Location",
+      mobileHeader: "Mobile",
+      emailHeader: "E-mail",
+      openingHeader: "Opening hours",
+      behindDAM: "(Behind the DAM)",
+
+      centralOpening: "Mon-Fri: 07:30 - 16:00",
+
+      miskolcOpening: "Mon-Thurs: 07:30 - 16:00",
+      miskolcPentek: "Fri: 07:30 - 14:00",
+      miskolcSzombat: "Sat 07:30 - 11:30",
+
+      debrecenOpening: "Mon-Fri: 8:00 - 16:00",
+      debrecenSzombat: "Sat: 8:00 - 12:00",
+
+      nyirOpening: "Mon-Fri: 8:00 - 16:30",
+      nyirSzombat: "Sat: 8:00 - 12:00",
+
+      bekesOpening: "Mon-Thurs: 7:30 - 16:00",
+      bekesPentek: "Fri: 7:30 - 15:30",
+
+      egerOpening: "Mon-Thurs: 7:30 - 16:00",
+      egerPentek: "Fri: 7:30 - 15:00",
+      
+      szegedOpening: "Mon-Thurs: 7:30 - 16:00",
+      szegedPentek: "Fri: 7:30 - 15:30",
+
+      contactsHeader: "Contacts",
+
+      contactsFirst: "Delta Metál KFT. proudly manufactures our products in Békéscsaba, at our site at Csorvási út 25. For the satisfaction of our customers, we offer a 2-year warranty on all our products. If you have any questions or would like to place an order, please contact us at the following contact details:",
+      contactsMobile: "Mobile:",
+      contactsLast: "Our colleagues are ready to provide you with the best advice and help. We warmly welcome you at Uniholz ​​KFT. in your team to help ensure the warmth and comfort of your home!",
+    },
+    hu: {
+      dropdownHun: "Magyar",
+      dropdownEng: "Angol",
+      navbarDropdown: "Nyelv",
+      navbarSellers: "Viszonteladók",
+      navbarContacts: "Elérhetőségek",
+      introHeader: "Rólunk",
+      introduction: "Üdvözöljük az Uniholz KFT. weboldalán! Az Uniholz KFT. a megbízhatóság és hosszú élettartam jegyében kínál olcsóbb megoldást az otthon melegéért. Szakértelmünkkel és több mint 25 éves tapasztalatunkkal a vegyes tüzelésű kazánok terén a legkiválóbb minőségű termékeket gyártjuk. A Delta vegyestüzelésű kazánokat kínáljuk, melyekkel a fűtés gondjait a legkisebb befektetéssel és a leggyorsabban oldhatjuk meg.",
+      boilerHeader: "Vegyestüzelésű Kazán Család",
+      Boilers: "A Delta vegyestüzelésű kazánok előnyei közé tartoznak a nagyméretű ajtók, amelyek megkönnyítik a kezelhetőséget. Takarékos megoldást kínálunk, hiszen ezek a kazánok nem csak hatékonyan fűtenek, hanem a leeső hamut is hasznosítják a fűtés során. Emellett az esztétikus külső is jellemzi termékeinket, így kazánjaink nem csak hatékonyak, de esztétikusak is a lakásban. A különböző típusok közül választhatja a D-28, D-36 és D-48 modelleket, melyek mindegyike kiválóan alkalmazkodik a különböző fűtési igényekhez. A vegyes tüzelésű kazánjainkat szivattyús és gravitációs fűtésre is használhatja, így biztos lehet abban, hogy megtalálja a számára legmegfelelőbb megoldást.",
+      sellersHeader: "Hivatalos viszonteladók",
+
+      Központitelep: "Uniholz Kft. - Központi telep",
+      Miskolcizelep: "Uniholz Kft. - Miskolci telep",
+      Debrecenitelep: "Keletker Vaskereskedés",
+      Nyíregyházitelep: "Nyír-Acél Kft.",
+      Békéscsabaitelep: "Delta-Metál Kft.",
+      Egritelep: "Uniholz Kft. - Egri telep",
+      Szegeditelep: "Hungaroferro Vaskereskedés",
+      locationHeader: "Helyszín",
+      mobileHeader: "Mobil",
+      emailHeader: "E-mail",
+      openingHeader: "Nyitva tartás",
+      behindDAM: "(A DAM mögött!)",
+
+      centralOpening: "H-P: 07:30 - 16:00",
+
+      miskolcOpening: "H-CS: 07:30 - 16:00",
+      miskolcPentek: "P: 07:30 - 14:00",
+      miskolcSzombat: "Szo: 07:30 - 11:30",
+
+      debrecenOpening: "H-P: 8:00 - 16:00",
+      debrecenSzombat: "Szo: 8:00 - 12:00",
+
+      nyirOpening: "H-P: 8:00 - 16:30",
+      nyirSzombat: "Szo: 8:00 - 12:00",
+
+      bekesOpening: "H-CS: 7:30 - 16:00",
+      bekesPentek: "P: 7:30 - 15:30",
+
+      egerOpening: "H-Cs: 7:30 - 16:00",
+      egerPentek: "P: 7:30 - 15:00",
+      
+      szegedOpening: "H-CS: 7:30 - 16:00",
+      szegedPentek: "P: 7:30 - 15:30",
+
+      contactsHeader: "Elérhetőségek",
+
+      contactsFirst: "A Delta Metál KFT. büszkén gyártja termékeinket Békéscsabán, a Csorvási út 25. szám alatt található telephelyünkön. Vásárlóink elégedettsége érdekében minden termékünkre 2 év garanciát vállalunk. Ha bármilyen kérdése van, vagy szeretne megrendelést leadni, kérjük, lépjen kapcsolatba velünk a következő elérhetőségeinken:",
+      contactsMobile: "Mobil:",
+      contactsLast: "Kollégáink készséggel állnak rendelkezésére a legjobb tanácsokkal és segítséggel. Várjuk Önt szeretettel az Uniholz KFT. csapatában, hogy segítsük otthona melege és kényelme érdekében!",
+      
+    }
+  };
+  
+  function changeLanguage(lang) {
+
+    dropdownHun.innerText = language[lang].dropdownHun;
+    dropdownEng.innerText = language[lang].dropdownEng;
+    navbarDropdown.innerText = language[lang].navbarDropdown;
+    navbarSellers.innerText = language[lang].navbarSellers;
+    navbarContacts.innerText = language[lang].navbarContacts;
+    introHeader.innerText = language[lang].introHeader;
+    introduction.innerText = language[lang].introduction;
+    boilerHeader.innerText = language[lang].boilerHeader;
+    Boilers.innerText = language[lang].Boilers;
+    sellersHeader.innerText = language[lang].sellersHeader;
+    
+    Központitelep.innerText = language[lang].Központitelep;
+    Miskolcizelep.innerText = language[lang].Miskolcizelep;
+    Debrecenitelep.innerText = language[lang].Debrecenitelep;
+    Nyíregyházitelep.innerText = language[lang].Nyíregyházitelep;
+    Békéscsabaitelep.innerText = language[lang].Békéscsabaitelep;
+    Egritelep.innerText = language[lang].Egritelep;
+    Szegeditelep.innerText = language[lang].Szegeditelep;
+
+    locationHeader.innerText = language[lang].locationHeader;
+    mobileHeader.innerText = language[lang].mobileHeader;
+    emailHeader.innerText = language[lang].emailHeader;
+    openingHeader.innerText = language[lang].openingHeader;
+    behindDAM.innerText = language[lang].behindDAM;
+
+    centralOpening.innerText = language[lang].centralOpening;
+
+    miskolcOpening.innerText = language[lang].miskolcOpening;
+    miskolcPentek.innerText = language[lang].miskolcPentek;
+    miskolcSzombat.innerText = language[lang].miskolcSzombat;
+
+    debrecenOpening.innerText = language[lang].debrecenOpening;
+    debrecenSzombat.innerText = language[lang].debrecenSzombat;
+
+    nyirOpening.innerText = language[lang].nyirOpening;
+    nyirSzombat.innerText = language[lang].nyirSzombat;
+
+    bekesOpening.innerText = language[lang].bekesOpening;
+    bekesPentek.innerText = language[lang].bekesPentek;
+
+    egerOpening.innerText = language[lang].egerOpening;
+    egerPentek.innerText = language[lang].egerPentek;
+
+    szegedOpening.innerText = language[lang].szegedOpening;
+    szegedPentek.innerText = language[lang].szegedPentek;
+
+    contactsHeader.innerText = language[lang].contactsHeader;
+
+    contactsFirst.innerText = language[lang].contactsFirst;
+    contactsMobile.innerText = language[lang].contactsMobile;
+    contactsLast.innerText = language[lang].contactsLast;
+
+  }
+  
+changeLanguage("hu");
+
+
+
+let SunOrMoon = true;
+function DarkAndLightMode(){
+    let icon = document.getElementById("LightOrDarkMode");
+
+    if (SunOrMoon == true) {
+        icon.className = "fa-solid fa-moon fa-2xl";
+        document.body.style.backgroundColor = "#303133";
+        document.body.style.color = "white";
+        SunOrMoon = false;   
+    }
+    else{
+        icon.className = "fa-solid fa-sun fa-2xl";
+        document.body.style.backgroundColor = "#C4A484";
+        document.body.style.color = "black";
+        SunOrMoon = true;
+    }
+}
+
